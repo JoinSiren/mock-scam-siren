a496d4e6893269508ddad8407d07d6f0ec19d96c
JoinSiren <support@joinsiren.com>
2025-12-31 15:44:21 +0530

    MockFinal2

--- src/IframeView.jsx
+++ src/IframeView.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function IframeView() {
+function IframeView({id="iFrame1"}) {
+function resizeIFrameToFitContent( iFrame ) {
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
 
+useEffect(() => {
+    var iFrame = document.getElementById( 'iFrame1' );
+    resizeIFrameToFitContent( iFrame );
+}, []);
   return (
     <div style={{ width: "100%" }}>
       <iframe
